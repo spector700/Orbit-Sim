d911371255101f03688f88f5a1a31fcf1813d16b
spector700 <72362399+spector700@users.noreply.github.com>
2023-10-02 10:46:38 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Solar";
+
+  # Flake inputs
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  # Flake outputs
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          let
+            # Use Python 3.11
+            python = pkgs.python311;
+          in
+          pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = [
+              # Python plus helper tools
+              (python.withPackages (ps: with ps; [
+                pip # The pip installer
+                pyglet
+              ]))
+            ];
+
+            shellHook = ''
+              echo "welcome to python" | ${pkgs.lolcat}/bin/lolcat
+            '';
+          };
+      });
+    };
+}
